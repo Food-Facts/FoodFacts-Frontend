b72544e5fe1e70b6dbd1e6eb8f5dd874e3fe95c6
Ernesto Casaverde <u20221b657@upc.edu.pe>
2024-11-19 21:29:01 -0500

    fix: routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,24 +1,20 @@
-import {RouterModule, Routes} from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './public/pages/home/home.component';
-import {ProductsComponent} from "./Food-Facts/products/products.component";
-import {NgModule} from "@angular/core";
-import {NutritionalValueComponent} from "./Food-Facts/nutritional-value/nutritional-value.component";
-import {NutritionistComponent} from "./Food-Facts/nutritionist/nutritionist.component";
-import {ProfileComponent} from "./public/pages/profile/profile.component";
-import {HealthRisksComponent} from "./Food-Facts/health-risks/health-risks.component";
-
+import { ProductsComponent } from "./Food-Facts/products/products.component";
+import { NgModule } from "@angular/core";
+import { NutritionalValueComponent } from "./Food-Facts/nutritional-value/nutritional-value.component";
+import { NutritionistComponent } from "./Food-Facts/nutritionist/nutritionist.component";
+import { ProfileComponent } from "./public/pages/profile/profile.component";
+import { HealthRisksComponent } from "./Food-Facts/health-risks/health-risks.component";
 
 export const routes: Routes = [
-
   { path: 'home', component: HomeComponent },
   { path: 'products', component: ProductsComponent },
-  {path:'nutritional-value/:id', component: NutritionalValueComponent  },
+  { path: 'nutritional-value/:id', component: NutritionalValueComponent },
   { path: '', redirectTo: '/products', pathMatch: 'full' },
-  {path: 'nutritionist', component: NutritionistComponent},
-  {path: 'profile', component: ProfileComponent},
-  {path: 'health-risks', component: HealthRisksComponent}
-  // Add other routes here
-
+  { path: 'nutritionist', component: NutritionistComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'healthRisk/:id', component: HealthRisksComponent } // Updated route definition
 ];
 
 @NgModule({
